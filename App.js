3345c9fb59b3f6440cf9feb6d295a9ebc12bc540
Sanchayan <sanchayang98@gmail.com>
2023-04-30 21:49:46 +0530

    Episode 03

--- App.js
+++ App.js
@@ -1,34 +1,54 @@
-// {/* <div id="parent">
-//   <div id="child">
-//     <h1>This is an h1</h1>
-//     <h2> This is an h2 </h2>
-//   </div>
-//   <div id="child2">
-//     <h1>This is an h1</h1>
-//     <h2> This is an h2 </h2>
-//   </div>
-// </div>; */}
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "Hello there, this is h1"),
-    React.createElement("h2", {}, "This is an h2")]
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    [React.createElement("h1", {}, "This is an h1"),
-    React.createElement("h2", {}, "This is an h2")]
-  )]
+// const heading = React.createElement("h1", {id:"heading"}, "Hi this is an H1 tag");
+// JSX This is not a JS, this is not a react code either, it a JSX code
+// So how JSX  code is getting converted to React code -> This conversion is done by BABEL
+// BABEL-> is a JS compiler that converts Jsx to React code
+// The below and above code are exactly the same
+
+// const jsxHeading = <h1 id="heading">Hi this is an H1 tag</h1>;
+
+// React components
+// -> There are 2 types of react components
+// Class based components => They are not used anymore
+// Functional component -> they are simple Js functions that returns a piece of JSX code/react element.
+// name of functional componentys starts with caps
+
+const heading = <h1>This is an h1 tag from react element</h1>;
+
+const Title = () => {
+  return <h1>This is Title component</h1>;
+};
+
+const Para = () => {
+  return <p>This is a paragraph component</p>;
+};
+
+// embedding a functional component inside a react elem
+
+const reactEle = (
+  <div>
+    <h1>This is react element</h1>
+    <Para />
+  </div>
 );
 
-//In order to simplify the above code we use JSX(Babel)
+// embedding react component inside another component
+// embedding react element inside a functional compnent -> we use {}
+// we can include any piece of JS code inside functional comp using {}
+// React Fragments -> They act as parent component and are not visible in html (<> </>)
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      {heading}
+      <h2 id="heading">This is h1 tag from React component</h2>
+      <Para />
+    </div>
+    <div></div>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log("End of program");
+root.render(<HeadingComponent />);
